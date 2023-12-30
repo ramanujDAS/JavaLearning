585e2455c6175686f7d49fc7435fe8be8470a0d5
ramanujsingh <ramanuj@gmail.com>
2023-12-30 13:42:35 +0530

    chsnge

---
+++ src/main/java/JVMOptimisationPerformance/EndOfChapterWorkspaces/Chapter05/ExploringMemory/src/Main.java
@@ -0,0 +1,17 @@
+package JVMOptimisationPerformance.EndOfChapterWorkspaces.Chapter05.ExploringMemory.src;
+
+public class Main {
+
+
+    public static void calculate(int calcValue) {
+        calcValue = calcValue * 100;
+    }
+
+    public static void main(String[] args) {
+        int localValue = 5;
+        calculate(localValue);
+        System.out.println(localValue);
+
+    }
+
+}
---
+++ src/main/java/JVMOptimisationPerformance/PerformanceExample1/src/Main.java
@@ -0,0 +1,13 @@
+package JVMOptimisationPerformance.PerformanceExample1.src;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(20000);
+        System.out.println("starting the work...");
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+        Integer max = Integer.parseInt(args[0]);
+        primeNumbers.generateNumbers(max);
+    }
+
+}
