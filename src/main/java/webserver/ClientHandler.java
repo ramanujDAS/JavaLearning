7fa18fe43c3a7c84d2c51d0073587d7c9ebaa212
ramanujsingh <ramanuj1st@gmail.com>
2024-03-25 13:23:25 +0530

    BASIC WEBSERVER TO FETCH FILE

--- src/main/java/webserver/ClientHandler.java
+++ src/main/java/webserver/ClientHandler.java
@@ -35,7 +35,7 @@ class ClientHandler implements Runnable {
 
     public void sendToOutputStream(Socket connection) throws IOException {
         try {
-            Thread.sleep(10);
+            Thread.sleep(2000);
             HttpResponse response = handleRequest(connection);
             connection.getOutputStream().write(((DefaultResponse.HTTPPROTOCOL
                     + getHttpCode(response)
